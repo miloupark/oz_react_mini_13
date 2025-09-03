f6fbc81bf5324d5ed34b168d2e5c53332813edd8
hyebinpark <hoonywhitesocks@gmail.com>
2025-09-03 14:43:14 +0900

    feat: Swiper 슬라이드 구현하기

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -2,22 +2,26 @@ import movieListData from "../data/movieListData.json";
 import MovieCard from "../components/MovieCard";
 import "../App.css";
 import { useState } from "react";
+import HeroSwiper from "../components/HeroSwiper";
 
 export default function App() {
   // movieListData의 results 배열로 접근
   const [movies] = useState(movieListData.results);
 
   return (
-    <section className="grid grid-cols-5 gap-5">
-      {movies.map((movie) => (
-        <MovieCard
-          key={movie.id}
-          movieId={movie.id}
-          imagePath={movie.poster_path}
-          movieTitle={movie.title}
-          movieRating={movie.vote_average}
-        />
-      ))}
-    </section>
+    <>
+      <HeroSwiper />
+      <section className="grid grid-cols-5 gap-5">
+        {movies.map((movie) => (
+          <MovieCard
+            key={movie.id}
+            movieId={movie.id}
+            imagePath={movie.poster_path}
+            movieTitle={movie.title}
+            movieRating={movie.vote_average}
+          />
+        ))}
+      </section>
+    </>
   );
 }
