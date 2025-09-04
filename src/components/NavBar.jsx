4a673e81a2d6fe4841f0586b66e5301bc7c042fb
hyebinpark <hoonywhitesocks@gmail.com>
2025-09-04 13:05:16 +0900

    chore: shadcn UI 설치 및 버튼 컴포넌트 테스트

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,11 +1,12 @@
 import { Link, NavLink } from "react-router-dom";
+import { Button } from "./ui/button";
 
 export default function NavBar() {
   return (
     <header className="flex justify-between py-2">
       {/* 로고 */}
       <Link to="/">
-        <h1>Movie</h1>
+        <h1>FilmArchive</h1>
       </Link>
 
       {/* 검색창 */}
@@ -23,9 +24,13 @@ export default function NavBar() {
       </form>
 
       {/* 로그인, 회원가입 */}
-      <nav>
-        <NavLink to="/login">로그인</NavLink>
-        <NavLink to="/signup">회원가입</NavLink>
+      <nav className="flex gap-2">
+        <Button variant="outline" asChild>
+          <NavLink to="/login">로그인</NavLink>
+        </Button>
+        <Button variant="outline" asChild>
+          <NavLink to="/signup">회원가입</NavLink>
+        </Button>
       </nav>
     </header>
   );
